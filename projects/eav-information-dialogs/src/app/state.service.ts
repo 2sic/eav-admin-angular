f9d37ed2062b761290ad50a82c4d7464019f9f1b
Roman Opalko <roman.opalko@2sic.com>
2019-03-20 11:14:10 +0100

    create default init values obj

--- projects/eav-information-dialogs/src/app/state.service.ts
+++ projects/eav-information-dialogs/src/app/state.service.ts
@@ -7,15 +7,95 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class StateService {
   constructor() { }
 
+  private initParams = {
+    sxcver: {
+      name: 'sxcVersion',
+      default: '00.00.00'
+    },
+    systype: {
+      name: 'sysType',
+      default: 'dnn'
+    },
+    sysver: {
+      name: 'sysVersion',
+      default: '00.00.00'
+    },
+    z: {
+      name: 'zoneId',
+      default: null
+    },
+    a: {
+      name: 'appId',
+      default: null
+    },
+    p: {
+      name: 'pageId',
+      default: null
+    },
+    i: {
+      name: 'instanceId',
+      default: null
+    },
+    c: {
+      name: 'contentBlockId',
+      default: null
+    },
+    d: {
+      name: 'debug',
+      default: false
+    },
+    lc: {
+      name: 'languagesContent',
+      default: []
+    },
+    lui: {
+      name: 'languagesUi',
+      default: []
+    },
+    lp: {
+      name: 'languagePrimary',
+      default: ''
+    },
+    rtt: {
+      name: 'tenantRoot',
+      default: ''
+    },
+    rtw: {
+      name: 'websiteRoot',
+      default: '/'
+    },
+    rta: {
+      name: 'appRoot',
+      default: ''
+    },
+    pop: {
+      name: 'partOfPage',
+      default: false
+    },
+    uid: {
+      name: 'userIsDesigner',
+      default: false
+    },
+    uic: {
+      name: 'userIsCoder',
+      default: false
+    },
+    uis: {
+      name: 'userIsSuperuser',
+      default: false
+    }
+  };
+
   saveParams(paramsObj: any) {
     Object.keys(paramsObj).map((key, index) => {
-      localStorage.setItem(key, paramsObj[key]);
+      localStorage.setItem(this.initParams[key].name, paramsObj[key]);
     });
   }
 
   getLocalStorageParams() {
+    console.log(localStorage);
     Object.keys(localStorage).map((key, index) => {
-      console.log(localStorage.getItem(key));
+      //console.log(localStorage.getItem(key));
     });
   }
 }
