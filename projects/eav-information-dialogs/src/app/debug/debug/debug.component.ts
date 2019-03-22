afc20a0644176cc5650d4ca7a106e7b7e2904d69
Roman Opalko <roman.opalko@outlook.com>
2019-03-22 14:08:36 +0100

    clea up

--- projects/eav-information-dialogs/src/app/debug/debug/debug.component.ts
+++ projects/eav-information-dialogs/src/app/debug/debug/debug.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StateService } from '../../state/state.service';
+import { Router, RouterEvent, NavigationStart } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-debug',
@@ -10,11 +12,13 @@ export class DebugComponent implements OnInit {
   localStorageArr: Array<any>;
 
   constructor(
-    private state: StateService
-  ) { }
-
-  ngOnInit() {
-    this.localStorageArr = this.state.getLocalStorageParams();
+    private state: StateService,
+    private router: Router
+  ) {
+    router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe((event: NavigationStart) => {
+      setTimeout(() => this.localStorageArr = this.state.getLocalStorageParams());
+    });
   }
-
 }
