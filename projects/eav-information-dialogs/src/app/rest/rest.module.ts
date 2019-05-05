d2ff9d9d0c48aeb44c2199ef4a2444d900e4735d
Raphael Müller <raaphii@gmail.com>
2019-05-05 15:23:10 +0200

    Make sure interceptor is configured in rest module

--- projects/eav-information-dialogs/src/app/rest/rest.module.ts
+++ projects/eav-information-dialogs/src/app/rest/rest.module.ts
@@ -1,5 +1,5 @@
 import { LibraryModule } from '../library/library.module';
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestRoutingModule } from './rest-routing.module';
 import { RestContentTypeComponent } from './content-type/content-type.component';
@@ -7,6 +7,17 @@ import { RestQueryComponent } from './query/query.component';
 import { RestItemComponent } from './item/item.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '../material/material.module';
+import { DnnInterceptor, RuntimeSettings } from '@2sic.com/dnn-sxc-angular';
+import { DnnDevSettings } from '../dev/dnn-dev-settings';
+import { environment } from '../../environments/environment';
+
+const providers: Provider[] = [
+  DnnInterceptor
+];
+
+if (!environment.production) {
+  providers.push({ provide: RuntimeSettings, useValue: DnnDevSettings });
+}
 
 @NgModule({
   declarations: [
@@ -20,6 +31,7 @@ import { MaterialModule } from '../material/material.module';
     MaterialModule,
     HttpClientModule,
     LibraryModule
-  ]
+  ],
+  providers
 })
 export class RestModule { }
